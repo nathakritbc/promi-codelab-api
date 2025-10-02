458252f7c3273fa8f9f666235b8f4f5c75a0f0e3
Jame Nathakrit <nathakritbc602@gmail.com>
2025-10-02 20:33:05 +0700

    feat: 🎸 Add HTTP Documentation for Promotion Rules
    - Introduced a new HTTP documentation file for promotion rules, detailing endpoints for login, creating, retrieving, updating, and deleting promotion rules.
    - Updated the `UpdatePromotionRuleDto` to simplify its structure by removing the OmitType, enhancing clarity and maintainability.

--- src/promotion-rules/adapters/inbounds/dto/updatePromotionRule.dto.ts
+++ src/promotion-rules/adapters/inbounds/dto/updatePromotionRule.dto.ts
@@ -1,4 +1,4 @@
-import { OmitType, PartialType } from '@nestjs/swagger';
+import { PartialType } from '@nestjs/swagger';
 import { CreatePromotionRuleDto } from './createPromotionRule.dto';
 
-export class UpdatePromotionRuleDto extends PartialType(OmitType(CreatePromotionRuleDto, ['promotionId'] as const)) {}
+export class UpdatePromotionRuleDto extends PartialType(CreatePromotionRuleDto) {}
